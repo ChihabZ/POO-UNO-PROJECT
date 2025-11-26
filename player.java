983e1619c01fdfaf147b6d9364ac640d84462799
chihabZ <chihabzaim12@gmail.com>
2025-11-26 01:06:32 +0100

    initializing player and GM

---
+++ player.java
@@ -0,0 +1,57 @@
+
+import java.util.List;
+import java.util.*
+import java.util.jar.Attributes;
+
+
+//donc hna tbda la classe player
+public class player {
+String name;
+List<Object> hand;
+
+//hna le constructeur
+public player(String name){
+    this.name = name;
+    hand = new ArrayList<>();
+
+}
+
+//hna les methods
+
+
+
+public String getname(){
+
+
+
+}
+
+public void addcard(card card){
+
+
+}
+
+public void removecard(card card){
+
+
+
+}
+
+public void showhand(){
+
+
+}
+
+public boolean winner{
+
+
+    
+}
+
+
+
+
+
+}
+
+
