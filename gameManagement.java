a8101d56c32e9f0bb5243be57eabbe2c8e1d738a
chihabZ <chihabzaim12@gmail.com>
2025-12-05 20:48:06 +0100

    fixed player 3la 7sab wahab

--- gameManagement.java
+++ gameManagement.java
@@ -10,12 +10,18 @@ public class GameManagement {
 
     private List<Player> players;
     private Deck deck;
-    private int direction = 1;   /
+    private int direction = 1;   
     private int current = 0;     
-    private Scanner sc = new Scanner(System.in);
+    private Scanner scan = new Scanner(System.in);
+
+
+
+
+
 
     
     // Constructeur t3 les joueues w g3 wch ys79o
+   
     public GameManagement(List<String> names) {
         deck = new Deck();
         players = new ArrayList<>();
@@ -29,116 +35,130 @@ public class GameManagement {
     }
 
     // voila le methode de distribution elewla g3
-    private void initialDeal() {
-        for (Player p : players) {
-            for (int i = 0; i < 7; i++) {
-                p.drawCard(deck);
-            }
-        }
-    }
+
+
+
+
 
    
-    private void flipFirstCard() {
-        repertoir_card first = deck.draw();
+   
+   
+    private void initialDeal() {
+       // 7cards each(tserbi)
+for (int i = 0; i < 7; i++) {
+    
 
-        // pour assurer bli manbdawch b +4
-        while (first.getType() == Type.plus4) {
-            deck.discard(first);
-            first = deck.draw();
-        }
 
-        deck.discard(first);
+}
+
+  }
+
+  
+  
+     private void flipFirstCard() {
+        
+
+
+
+
+    
+     }
+
+        
+
+
+
+
+
     }
 
     
-    // play ur turn sma fiha pioche w discard w effect
-    public void playTurn(int cardIndex) {
-        Player p = players.get(current);
-        repertoir_card top = deck.topDiscard();
-
-        repertoir_card played = p.playCard(cardIndex, top);
-
-        if (played == null) {
-            System.out.println(p.getName() + " pioche une carte");
-            p.drawCard(deck);
-        } else {
-            deck.discard(played);
-            applyEffect(played);
-        }
-
-        nextPlayer();
-    }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 
     
+   
+   
+   
+   
+   
+   
+   
+   
     // the effect ta3na sma ya effet 3la le prochain ya effet ela jeu g3
     private void applyEffect(repertoir_card card) {
 
-        switch (card.getType()) {
-            case skip -> nextPlayer();
-            case reverse -> direction *= -1;
-            case plus2 -> {
-                nextPlayer();// hdi sma 3ndna double nextplayer bch t3 +2 mayl3bch
-                players.get(current).drawCard(deck);
-                players.get(current).drawCard(deck);
-            }
-            case plus4 -> {
-                nextPlayer();//same as +2, 7bibna mayl3bch
-                for (int i = 0; i < 4; i++) players.get(current).drawCard(deck);
-                chooseWildColor(card);
-            }
-            case chng -> chooseWildColor(card);  
-            // hna c'est une peu speciale, nkhyro la couleur mor discard d'un wild 
-            default -> {}
-        }
+       
     }
 
-    // ----------------------------
-    // Choix de couleur pour Wild / +4
+
+   
+    // choix de couleur pour wild+4
     private void chooseWildColor(repertoir_card card) {
-        System.out.println("played a wild card ! choose a color :");
-        System.out.println("0 : RED, 1 : BLUE, 2 : YELLOW, 3 : GREEN");
+       
+       
+    }
 
-        int colorChoice = sc.nextInt();
-        switch (colorChoice) {
-            case 0 -> card.setColour(Colour.red);
-            case 1 -> card.setColour(Colour.blue);
-            case 2 -> card.setColour(Colour.yellow);
-            case 3 -> card.setColour(Colour.green);
-            default -> card.setColour(Colour.red);
-        }
 
-        System.out.println("La couleur choisie est : " + card.getColour());
+    private void nextPlayer() {
+
+       
+
     }
 
    
-    private void nextPlayer() {
-        current = (current + direction + players.size()) % players.size();
+    public Player getCurrentPlayer() {
+
+       
     }
 
-    // ----------------------------
-    public Player getCurrentPlayer() {
-        return players.get(current);
-    }
+
+
+
+
+
 
     public RepertoireCard getTopDiscard() {
-        return deck.topDiscard();
+       
+
     }
+
+
+
+
+
+
 
     public boolean isGameOver() {
-        for (Player p : players) {
-            if (p.getHand().isEmpty()) return true;
-        }
-        return false;
+        
+
+
     }
 
+
+
+
+
+
+
     public void announceWinner() {
-        for (Player p : players) {
-            if (p.getHand().isEmpty()) {
-                System.out.println("and the winner is : " + p.getName() + " !");
-                return;
-            }
-        }
+       
+
+
     }
+
+
+
 }
 
 
--- player.java
+++ player.java
@@ -1,48 +1,87 @@
 
-import java.util.List;
 import java.util.*;
 import java.util.jar.Attributes;
 
+import javax.smartcardio.Card;
 
 //donc hna tbda la classe player
 public class player {
-String name;
-List<Object> hand;
 
-//hna le constructeur
+private String name;
+private List<Card> hand;
+private Scanner scan = new Scanner(System.in);
+private deck = new Deck();
+
+//constructeur
 public player(String name){
-    this.name = name;
-    hand = new ArrayList<>();
 
-}
-
-//hna les methods
-
-
-
-public String getname(){
-
+this.name = name;
+this.hand = new ArrayList<>();
 
 
 }
 
-public void addcard(card card){
+//now the methods
 
+public void draw(int n){
 
-}
-
-public void removecard(card card){
-
-
-
-}
-
-public void showhand(){
-
+    card c = deck.drawplus(n);
+    hand.add(c);
 
 }
 
 
+public void showcards(Deck deck){
+
+System.out.println("\n" + name + " hand :");
+for(int i = 0 ; i < hand.size() ; i++){
+
+System.out.println((i+1) + ": " + hand.get(i));
+
+}
+System.out.println("choose a card to play, or press 0 to draw");
+int index = scan.nextInt();
+if(index == 0){
+
+draw(1);
+
+}else{
+
+index--;// for machine array
+
+if(index < 0 || index >= hand.size()){
+
+System.out.println("invalid");
+return;
+
+}else{
+card c = hand.get(index);
+
+if(!c.allowplay(deck.discadpile(0))){
+
+System.out.println("you cant play this card");
+return;
+
+}else{
+    deck.discadpile.add(c);
+    hand.remove(index);
+    System.out.println(name + " played " + c);
+}
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+//and from here nroho to the main(),
+//not sure if all classes are here so ill be working au fur et a mesure
 }
 
 
