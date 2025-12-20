9006b9a06a3d96e3cf1539da744455de5d554ca3
shyraz <shyrazguedd.pro@gmail.com>
2025-12-20 22:10:38 +0100

    ajout du dessin de back of card

--- main.java
+++ main.java
@@ -1,23 +1,41 @@
+import java.util.Scanner;
+
 public class main {
-    
+     private static String[][] backofcard = new String[9][8];
+
+static {
+    // Haut et bas
+    for (int i = 0; i < 8; i++) {
+        backofcard[0][i] = "-";
+        backofcard[8][i] = "-";
+    }
+
+    // Côtés
+    for (int j = 0; j < 9; j++) {
+        backofcard[j][0] = "|";
+        backofcard[j][7] = "|";
+    }
+    for (int i = 1; i <= 7; i++) {
+    backofcard[i][2] = "U";
+    backofcard[i][3] = "N";
+    backofcard[i][4] = "N";
+    backofcard[i][5] = "O";
+}
+}
+public static void displaybackofcard(){
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 8; j++) {
+            System.out.print(backofcard[i][j]);
+        }
+        System.out.println();
+    }
+}
+
+
+
     
     public static void main(String[] args) {
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
         //menu switch 
         System.out.println("Welcome to the UNO card game!");
