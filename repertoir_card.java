2eddcc971957a7fb69f3c5a008c4296e41177e66
shyraz <shyrazguedd.pro@gmail.com>
2025-12-23 18:28:57 +0100

    0 probleme dans repertoir

--- repertoir_card.java
+++ repertoir_card.java
@@ -5,11 +5,7 @@ import java.util.Stack;
 
 public class repertoir_card   {
     private color colour;
-<<<<<<< HEAD
-    private Type type;
-=======
     private type type;
->>>>>>> ae7d7c84a80714192c5827c22b2c8150bff8d7c8
     private int number;
     
 
@@ -29,17 +25,10 @@ public ArrayList<card> initCard() {
 
     // Regular cards 
     //enum colour
-<<<<<<< HEAD
-    Color[] colours = {Color.red, Color.blue, Color.yellow, Color.green, Color.WILD};
-    Type[] types = {Type.regular,Type.skip, Type.reverse, Type.plus2,Type.wild_4_plus,Type.wild};
-    for (Colour c : colours ) {
-        if (c!= Colour.WILD){
-=======
     color[] colours = {color.red, color.blue, color.yellow, color.green, color.WILD};
-    type[] types = { type.REVERSE,type.SKIP, type.PLUS2,type.wild_4_plus,type.wild};
+    type[] types = {type.SKIP, type.REVERSE, type.PLUS2,type.wild_4_plus,type.wild};
     for (color c : colours ) {
         if (c!= color.WILD){
->>>>>>> ae7d7c84a80714192c5827c22b2c8150bff8d7c8
           for (int i = 0; i <= 9; i++) {
               if (i==0){
             cartes.add(new regular(c,i));
@@ -59,8 +48,8 @@ public ArrayList<card> initCard() {
        for (type t : types) {
           if (c !=color.WILD && t !=type.wild && t !=type.wild_4_plus) {
         
-            cartes.add(new nonregular(c, t));
-             cartes.add(new nonregular(c, t));
+            cartes.add(new nonregular(c, t,-1));
+             cartes.add(new nonregular(c, t,-1));
            }
          }
      }
@@ -74,12 +63,12 @@ public ArrayList<card> initCard() {
 }
 //======================================================
     }
-public List<card>getcartes()
-{
-    return cartes;
-}
+// public List<card>getcartes()
+// {
+//     return cartes;
+// }
 
-}  
+
 
 
 
