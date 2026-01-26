7b470653b70f4f751a715462fd6d66acd504110f
shyraz <shyrazguedd.pro@gmail.com>
2026-01-26 21:45:52 +0100

    announce winner

--- gameManagement.java
+++ gameManagement.java
@@ -1,7 +1,4 @@
 
-
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,79 +6,50 @@ import java.util.Scanner;
 public class gameManagement {
 
     private final List<player> players;
-    private  deck deck;
-    private int direction = 1;   
-    private int current = 0;     
+    private deck deck;
+    private int direction = 1;
+    private int current = 0;
     private Scanner scan = new Scanner(System.in);
 
-
-
-
-
-
-    
     // Constructeur t3 les joueues w g3 wch ys79o
-   
+
     public gameManagement(List<String> names) {
         players = new ArrayList<>();
         deck = new deck();
         for (String n : names) {
             players.add(new player(n, deck));
-        
 
-        }        
+        }
         initialDeal();
-                         // distribuer 7 cartes q chaque player
-                       // first card after initializing goes to the table, a part +4 ofc
+        // distribuer 7 cartes q chaque player
+        // first card after initializing goes to the table, a part +4 ofc
     }
 
+    public gameManagement() {
 
+        players = new ArrayList<>();
+        deck = new deck();
+        players.add(new player("player", deck));
+        players.add(new bot("BOT", deck));
+        initialDeal();
 
-public gameManagement(){
-    
- players = new ArrayList<>();
- deck = new deck();
- players.add(new player("player", deck));
- players.add(new bot("BOT", deck));
- initialDeal();
-
-}
-
-
-
+    }
 
     // voila le methode de distribution elewla g3
 
-
-
-
-
-   
-   
-   
     private void initialDeal() {
-       // 7cards each(tserbi)
+        // 7cards each(tserbi)
 
- for (int index = 0; index < players.size(); index++) {
-    
- 
-       for (int i = 0; i < 7; i++) {
-    
-    players.get(index).draw(1);
+        for (int index = 0; index < players.size(); index++) {
 
-       }
-       }
-        }//end class initialDeal
+            for (int i = 0; i < 7; i++) {
 
+                players.get(index).draw(1);
 
-  
-     
+            }
+        }
+    }// end class initialDeal
 
-        
-
-   
-   
-   
     // the effect ta3na sma ya effet 3la le prochain ya effet ela jeu g3
     public void applyEffect(card card) {
         if (!(card instanceof nonregular)) {
@@ -89,125 +57,119 @@ public gameManagement(){
         }
         nonregular nr = (nonregular) card;
 
-       if(nr.gettype() == type.SKIP){
+        if (nr.gettype() == type.SKIP) {
 
-           nextPlayer();
-         }else if(nr.gettype() == type.REVERSE){
-              direction *= -1;
-            }else if(nr.gettype()==type.PLUS2){
-              nextPlayer();
-              getCurrentPlayer().draw(2);
-            }else if(nr.gettype()==type.wild_4_plus){
-            chooseWildColor(card,getCurrentPlayer()); // modif-----------------------------
-             nextPlayer();
-              getCurrentPlayer().draw(4);
-              
-            }else if(nr.gettype() == type.wild){
-              chooseWildColor(card,getCurrentPlayer());// modif-----------------------------
-               
-            } 
-    
-     }
+            nextPlayer();
+        } else if (nr.gettype() == type.REVERSE) {
+            direction *= -1;
+        } else if (nr.gettype() == type.PLUS2) {
+            nextPlayer();
+            getCurrentPlayer().draw(2);
+        } else if (nr.gettype() == type.wild_4_plus) {
+            chooseWildColor(card, getCurrentPlayer()); // modif-----------------------------
+            nextPlayer();
+            getCurrentPlayer().draw(4);
 
+        } else if (nr.gettype() == type.wild) {
+            chooseWildColor(card, getCurrentPlayer());// modif-----------------------------
 
-   
+        }
+
+    }
+
     // choix de couleur pour wild+4
-    public void chooseWildColor(card card,player player) {
-        //ajout----------------------------------------------------------------
-       if (player instanceof bot){
-              ((bot)player).chooseColor(card);
-       }else{
-    System.out.println("choose a color: 1.RED 2.BLUE 3.GREEN 4.YELLOW");
-    int choice = scan.nextInt();
-    switch(choice){
-        case 1:
-        card.setColor(color.red);
-        break;
-        case 2:
-        card.setColor(color.blue);
-        break;
-        case 3:
-        card.setColor(color.green);
-        break;
-        case 4:
-        card.setColor(color.yellow);
-        break;
-        default:
-        System.out.println("invalid choice, color set to RED by default");
-        card.setColor(color.red);
-        break;
-       
-    }}
-    // if (player instanceof bot) {
+    public void chooseWildColor(card card, player player) {
+        // ajout----------------------------------------------------------------
+        if (player instanceof bot) {
+            ((bot) player).chooseColor(card);
+        } else {
+            System.out.println("choose a color: 1.RED 2.BLUE 3.GREEN 4.YELLOW");
+            int choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    card.setColor(color.red);
+                    break;
+                case 2:
+                    card.setColor(color.blue);
+                    break;
+                case 3:
+                    card.setColor(color.green);
+                    break;
+                case 4:
+                    card.setColor(color.yellow);
+                    break;
+                default:
+                    System.out.println("invalid choice, color set to RED by default");
+                    card.setColor(color.red);
+                    break;
 
-    // }
-}// end choose wild color
+            }
+        }
+        // if (player instanceof bot) {
 
+        // }
+    }// end choose wild color
 
     public void nextPlayer() {
         current = (current + direction + players.size()) % players.size();
 
-
-       
-
     }// end next player
 
-   
     public player getCurrentPlayer() {
         return players.get(current);
     }// end get current player
 
-
-    public card getTopDiscard() { 
+    public card getTopDiscard() {
         return deck.getTopDiscard();
-       
 
     }// end get top discard
 
-
-    public boolean isGameOver() { 
+    public boolean isGameOver() {
         for (player p : players) {
             if (p.hasEmptyHand()) {
                 return true;
             }
         }
         return false;
-        
-
 
     }// end isGameOver
 
-
-
-
-
-
-
-    public void announceWinner() { 
+    public void announceWinner() {
         for (player p : players) {
             if (p.hasEmptyHand()) {
-                System.out.println("The winner is " + p.getName() + "!");
+                System.out.println("                    ╔═════════════════════════════════════════════════════╗");
+                System.out.println("                    ║                                                     ║");
+                System.out.println("                    ║   ███████      ███      ███       ███    ████████   ║");
+                System.out.println("                    ║  ██           █   █     ██ █     █ ██    ██         ║");
+                System.out.println("                    ║  ██          █     █    ██  █   █  ██    ████████   ║");
+                System.out.println("                    ║  ██    ██   █████████   ██   █ █   ██    ██         ║");
+                System.out.println("                    ║   ███████  ██       ██  ██   ███   ██    ████████   ║");
+                System.out.println("                    ║                                                     ║");
+                System.out.println("                    ║                       O V E R                       ║");
+                System.out.println("                    ║                                                     ║");
+                System.out.println("                    ╚═════════════════════════════════════════════════════╝");
+                System.out.println("THE WINNER IS " + p.getName() + "!");
                 return;
             }
         }
-       
 
-
-    }//end announce winner
+    }// end announce winner
 
     public deck getDeck() {
         return deck;
     }
 }
 
-
-
-/*anyone checking this code should know that game management is not the main game,
-we built gamemanagement to ease up the manipulation of the player table in the main 
-so its as complementary as the deck and card classes, mostly manipiulating the deck and discard piles
-and also initial deal and flipping the first card on the table, as well as initiating the components 
-of the table management itself. */
-//long story short, this is not the main class but a complementary one to help us manage the game better.
-
-
-
+/*
+ * anyone checking this code should know that game management is not the main
+ * game,
+ * we built gamemanagement to ease up the manipulation of the player table in
+ * the main
+ * so its as complementary as the deck and card classes, mostly manipiulating
+ * the deck and discard piles
+ * and also initial deal and flipping the first card on the table, as well as
+ * initiating the components
+ * of the table management itself.
+ */
+// long story short, this is not the main class but a complementary one to help
+// us manage the game better.
--- main.java
+++ main.java
@@ -3,8 +3,7 @@ import java.util.List;
 
 public class main {
 
-
-// public class WelcomeUno {
+    // public class WelcomeUno {
 
     // ANSI colors
     public static final String RESET = "\u001B[0m";
@@ -17,7 +16,7 @@ public class main {
 
     public static void showWelcome() {
 
-        System.out.println("                    ╔═══════════════════════════════════════════╗");
+        System.out.println("                    ╔════════════════════════════════════════════╗");
         System.out.println("                    ║                                            ║");
         System.out.println("                    ║        ██    ██ ██     ██  ██████          ║");
         System.out.println("                    ║        ██    ██ ████   ██ ██    ██         ║");
@@ -26,118 +25,118 @@ public class main {
         System.out.println("                    ║         ██████  ██   ████  ██████          ║");
         System.out.println("                    ║                                            ║");
         System.out.println("                    ║                                            ║");
-        System.out.println("                    ║ "         +RED+ "           W E L C O M E"+ BLUE+"   T O " +WHITE+"            ║");
-        System.out.println("                    ║             " +GREEN+ " T H E   G A M E" +WHITE+"               ║");
+        System.out.println("                    ║ " + RED + "           W E L C O M E" + BLUE + "   T O " + WHITE
+                + "            ║");
+        System.out.println(
+                "                    ║             " + GREEN + " T H E   G A M E" + WHITE + "               ║");
         System.out.println("                    ║                                            ║");
-        System.out.println("                    "+WHITE+"║"+"    " + RED + "■■" + GREEN + "■■" + YELLOW + "■■" + BLUE + "■■" + YELLOW + "   LET'S PLAY!   " +
-                           BLUE + "■■" + YELLOW + "■■" + GREEN + "■■" + RED + "■■" + WHITE + "       ║");
-                          
+        System.out.println("                    " + WHITE + "║" + "    " + RED + "■■" + GREEN + "■■" + YELLOW + "■■"
+                + BLUE + "■■" + YELLOW + "   LET'S PLAY!   " +
+                BLUE + "■■" + YELLOW + "■■" + GREEN + "■■" + RED + "■■" + WHITE + "       ║");
+
         System.out.println("                    ║                                            ║");
         System.out.println("                    ╚════════════════════════════════════════════╝" + RESET);
     }
 
-    
     public static void main(String[] args) {
 
-     List<String> players = new ArrayList<>();
-
-
+        List<String> players = new ArrayList<>();
 
         showWelcome();
-        //menu switch 
-        //System.out.println("                          WELCOME TO THE UNO CARD CONSOLE GAME!");
-        System.out.print("                            ==========  MENU  ===========        \n                                    1.Multiplayer\n                                    2.Singleplayer\n  Choose an option: ");
-        
+        // menu switch
+        // System.out.println(" WELCOME TO THE UNO CARD CONSOLE GAME!");
+        System.out.print(
+                "                            ==========  MENU  ===========        \n                                    1.Multiplayer\n                                    2.Singleplayer\n  Choose an option: ");
+
         int option;
         java.util.Scanner scanner = new java.util.Scanner(System.in);
-        option = scanner.nextInt(); 
-        switch(option) {
+        option = scanner.nextInt();
+        switch (option) {
             case 1:
                 System.out.println("   Multiplayer mode selected.");
-            System.out.println("   How many players ? ");
-            int numPlayers = scanner.nextInt();    
-            for (int i = 1; i <= numPlayers; i++) {
-                System.out.print("   Enter name for Player " + i + ": ");
-                String playerName = scanner.next();
-                players.add(playerName);
-            }
-            gameManagement game = new gameManagement(players);
-            
-            // n7ato la 1ere carte f discard pile
-            card firstCard = game.getDeck().drawplus(1);
-            game.getDeck().addtodiscard(firstCard);
-            System.out.println("   First card on discard:");
-            firstCard.logo();
-            if (firstCard instanceof nonregular) {
-                nonregular nr = (nonregular) firstCard;
-                if (nr.gettype() == type.wild || nr.gettype() == type.wild_4_plus) {
-                    game.chooseWildColor(firstCard, game.getCurrentPlayer());//modif------------------------------
+                System.out.println("   How many players ? ");
+                int numPlayers = scanner.nextInt();
+                for (int i = 1; i <= numPlayers; i++) {
+                    System.out.print("   Enter name for Player " + i + ": ");
+                    String playerName = scanner.next();
+                    players.add(playerName);
                 }
-            }
-            
-            // Game loop where players take turns w kda
-            while (!game.isGameOver()) {
-                player current = game.getCurrentPlayer();
-                System.out.println("\n   --- " + current.getName() + "'s turn ---");
-                
-                // Show top discard
-                card topDiscard = game.getTopDiscard();
-                System.out.println("   Top discard:");
-                topDiscard.logo();
-                
-                // hna on controle le tour pour chaque joueur
-                card playedCard = current.showcards(game.getDeck());
-                if (playedCard != null) {
-                    game.applyEffect(playedCard);
+                gameManagement game = new gameManagement(players);
+
+                // n7ato la 1ere carte f discard pile
+                card firstCard = game.getDeck().drawplus(1);
+                game.getDeck().addtodiscard(firstCard);
+                System.out.println("   First card on discard:");
+                firstCard.logo();
+                if (firstCard instanceof nonregular) {
+                    nonregular nr = (nonregular) firstCard;
+                    if (nr.gettype() == type.wild || nr.gettype() == type.wild_4_plus) {
+                        game.chooseWildColor(firstCard, game.getCurrentPlayer());// modif------------------------------
+                    }
                 }
-                
-                game.nextPlayer();
-            }
-            
-            game.announceWinner();
-            break;
-        
-          case 2:
-    System.out.println("   Singleplayer mode selected.");
-    
-    gameManagement game2 = new gameManagement();
-    
-    // n7ato la 1ere carte f discard pile
-    card firstCard2 = game2.getDeck().drawplus(1);
-    game2.getDeck().addtodiscard(firstCard2);
-    System.out.println("   First card on discard:");
-    firstCard2.logo();
-    if (firstCard2 instanceof nonregular) {
-        nonregular nr = (nonregular) firstCard2;
-        
-        if (nr.gettype() == type.wild || nr.gettype() == type.wild_4_plus) {
-            game2.chooseWildColor(firstCard2, game2.getCurrentPlayer());//modif=----------------------------------
+
+                // Game loop where players take turns w kda
+                while (!game.isGameOver()) {
+                    player current = game.getCurrentPlayer();
+                    System.out.println("\n   --- " + current.getName() + "'s turn ---");
+
+                    // Show top discard
+                    card topDiscard = game.getTopDiscard();
+                    System.out.println("   Top discard:");
+                    topDiscard.logo();
+
+                    // hna on controle le tour pour chaque joueur
+                    card playedCard = current.showcards(game.getDeck());
+                    if (playedCard != null) {
+                        game.applyEffect(playedCard);
+                    }
+
+                    game.nextPlayer();
+                }
+
+                game.announceWinner();
+                break;
+
+            case 2:
+                System.out.println("   Singleplayer mode selected.");
+
+                gameManagement game2 = new gameManagement();
+
+                // n7ato la 1ere carte f discard pile
+                card firstCard2 = game2.getDeck().drawplus(1);
+                game2.getDeck().addtodiscard(firstCard2);
+                System.out.println("   First card on discard:");
+                firstCard2.logo();
+                if (firstCard2 instanceof nonregular) {
+                    nonregular nr = (nonregular) firstCard2;
+
+                    if (nr.gettype() == type.wild || nr.gettype() == type.wild_4_plus) {
+                        game2.chooseWildColor(firstCard2, game2.getCurrentPlayer());// modif=----------------------------------
+                    }
+                }
+
+                // Game loop where players take turns w kda
+                while (!game2.isGameOver()) {
+                    player current = game2.getCurrentPlayer();
+                    System.out.println("\n   --- " + current.getName() + "'s turn ---");
+
+                    // Show top discard
+                    card topDiscard2 = game2.getTopDiscard();
+                    System.out.println("   Top discard:");
+                    topDiscard2.logo();
+
+                    // hna on controle le tour pour chaque joueur
+                    card playedCard2 = current.showcards(game2.getDeck());
+                    if (playedCard2 != null) {
+                        game2.applyEffect(playedCard2);
+                    }
+
+                    game2.nextPlayer();
+                }
+
+                game2.announceWinner();
+                break;
         }
     }
-    
-    // Game loop where players take turns w kda
-    while (!game2.isGameOver()) {
-        player current = game2.getCurrentPlayer();
-        System.out.println("\n   --- " + current.getName() + "'s turn ---");
-        
-        // Show top discard
-        card topDiscard2 = game2.getTopDiscard();
-        System.out.println("   Top discard:");
-        topDiscard2.logo();
-        
-        // hna on controle le tour pour chaque joueur
-        card playedCard2 = current.showcards(game2.getDeck());
-        if (playedCard2 != null) {
-            game2.applyEffect(playedCard2);
-        }
-        
-        game2.nextPlayer();
-    }
-    
-    game2.announceWinner();
-    break;  
-}
-    }
 
-}// ya khawti rni khaletha, zdt bzaf 3fays hhhhhh mais mzal ma drt bot 
-
+}// ya khawti rni khaletha, zdt bzaf 3fays hhhhhh mais mzal ma drt bot
